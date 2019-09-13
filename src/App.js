b66806c5c9cc1b3679d1d9a8b8fc85afc6af8847
pscroce <pscroce@gmail.com>
2019-09-12 21:53:42 -0400

    feat: add placeholder landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <body>
+        <div className="welcome-section">
+
+          <div className="section -paragraph">
+            <h1>Reversible</h1>
+            <h2>Tools for reversing climate change</h2>
+            <p>Reversible is an organization building open source software products to facilitate the removal of carbon dioxide from the atmosphere and reverse climate change.</p>
+            <p>Data analysts, designers, engineers, product managers, project managers, and fellow travelers of all stripes. Each craft extremely is powerful. Together, they have upended industry after industry.</p>
+            <p>Now we're turing our tools on a formidable and surmountable scope of work: reversing climate change. It is the challenge most worthy of our attention.</p>
+          </div>
+
+        </div>
+
+      </body>
+
+      </div>
+    );
+  }
 }
 
 export default App;
