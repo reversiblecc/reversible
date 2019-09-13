5ccf0a1bf44787fb956a9cd9a24c50acbfbf7bd6
pscroce <pscroce@gmail.com>
2019-09-12 22:41:55 -0400

    feat: add logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import logo from './logo.png';
 
 
 class App extends Component {
@@ -12,7 +12,10 @@ class App extends Component {
         <div className="welcome-section">
 
           <div className="section -paragraph">
+          <div className="logo-section">
+            <img src={logo} alt="The Reversible logo, a small blue dot" id="logo" />
             <h1>Reversible</h1>
+          </div>
             <h2>Tools for reversing climate change</h2>
             <p>Reversible is an organization building open source software products to facilitate the removal of carbon dioxide from the atmosphere and reverse climate change.</p>
             <p>Data analysts, designers, engineers, product managers, project managers, and fellow travelers of all stripes. Each craft extremely is powerful. Together, they have upended industry after industry.</p>
