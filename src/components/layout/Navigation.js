088c2c667154addb06170b9fa8859c5396a18229
pscroce <pscroce@gmail.com>
2019-09-29 18:53:18 -0400

    feat: adjust navigation styles and add Footer component

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -12,6 +12,9 @@ function Navigation() {
         <li className="nav-item">
           <Link to="/calculator">Carbon Calculator</Link>
         </li>
+        <li className="nav-item">
+          <a href="https://github.com/reversiblecc/baseline-proxy" target="_blank" rel="noopener noreferrer">Baseline Proxy</a>
+        </li>
 
         <div className="nav-right">
 
