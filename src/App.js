1be0b3ef7757208df45a9ec68b894d9fe25ba448
pscroce <pscroce@gmail.com>
2019-09-30 22:47:05 -0400

    feat: add Source Library

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ScrollToTop from 'react-router-scroll-top'
 
 import Home from './components/pages/Home';
 import Calculator from './components/pages/Calculator';
+import SourceLibrary from './components/pages/SourceLibrary';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route path='/calculator' component={Calculator} />
+              <Route path='/source-library' component={SourceLibrary} />
               <Route component={Home} />
             </Switch>
           </div>
--- src/components/pages/SourceLibrary.js
+++ src/components/pages/SourceLibrary.js
@@ -8,12 +8,12 @@ import wordmark from '../../wordmark.svg';
 
 import ReactGA from 'react-ga';
 
-class Home extends Component {
+class SourceLibrary extends Component {
   render() {
     ReactGA.initialize('UA-148187805-1');
-    ReactGA.pageview('/');
+    ReactGA.pageview('/source-library');
     return (
-      <div className="Home">
+      <div className="SourceLibrary">
 
       <Navigation />
         <div className="main-section">
@@ -23,15 +23,22 @@ class Home extends Component {
           <Link to="/"><img src={logo} alt="The Reversible logo, a small blue dot" id="logo" />
           <img src={wordmark} alt="The Reversible wordmark" id="wordmark" /></Link>
           </div>
-            <h1>Software for reversing climate change</h1>
+            <h1>Source Library</h1>
             <p>Reversible is making open source software products to facilitate the removal of carbon dioxide from the atmosphere and reverse climate change.</p>
+            <p>We publish all our source code here for free community use.</p>
           </div>
 
           <div className="section -paragraph">
-            <h2>Tools</h2>
+            <h2>Browse the library</h2>
               <ul>
-                <li><Link to="/calculator">Carbon Calculator</Link></li>
-                <li><Link to="/source-library">Source Library</Link></li>
+              <li>
+                <a href="https://github.com/reversiblecc/reversible/blob/master/src/components/pages/Calculator.js">Carbon Calculator source</a>
+              </li>
+              <li>
+                <a href="https://github.com/reversiblecc/reversible">Reversible website source</a>
+              </li>
+
+
               </ul>
           </div>
 
@@ -42,4 +49,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default SourceLibrary;
